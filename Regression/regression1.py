a96c8cc0a289001ad72b384646f095a9ad540b11
Jay Landers <jay@jaybirdgroup.net>
2018-01-25 22:31:12 -0700

    Implemented sklearn linear regression model

--- Regression/regression1.py
+++ Regression/regression1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import quandl, math
 import numpy as np
-from sklearn import preprocessing, cross_validation, svm
+from sklearn import preprocessing, model_selection, svm
 from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
@@ -26,5 +26,10 @@ y = np.array(df['label'])
 
 X = preprocessing.scale(X)
 
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size = 0.2)
 
-print(len(X), len(y))
+clf = LinearRegression()
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
