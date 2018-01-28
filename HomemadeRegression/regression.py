e90e8971bd122f7ea1c42f4f2df4bd7880fb9629
Jay Landers <jay@jaybirdgroup.net>
2018-01-28 11:38:56 -0700

    Added documentation and dataset creation

--- HomemadeRegression/regression.py
+++ HomemadeRegression/regression.py
@@ -2,14 +2,30 @@ from statistics import mean
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
+import random
 
 style.use('ggplot')
 
-xs = np.array([1, 2, 3, 4, 5, 6], dtype=np.float64)
-ys = np.array([5, 4, 6, 5, 6, 7], dtype=np.float64)
+
+def create_dataset(size, variance, step=1, correlation=None):
+    """Create a dataset of arrays of x and y values"""
+    val = 1
+    ys = []
+    for i in range(size):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+
+    xs = [i for i in range(size)]
+
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 
 def best_fit_slope_yint(xs, ys):
+    """Find the slope and y-intercept of the line of best fit (linear regression) of the dataset"""
     meanx = mean(xs)
     meany = mean(ys)
 
@@ -28,6 +44,8 @@ def best_fit_slope_yint(xs, ys):
 
 
 def evaluate_model(model, original_line):
+    """Evaluate the linear regression model using the coefficient of determination"""
+
     meany = mean(original_line)
 
     squared_error_model = sum((model-original_line)**2)
@@ -37,16 +55,23 @@ def evaluate_model(model, original_line):
     return r_squared
 
 
+# Creating the dataset of x and y values
+xs, ys = create_dataset(50, 25, 3, 'pos')
+
+# Get the line of best fit(linear regression model) of the dataset
 m, b = best_fit_slope_yint(xs, ys)
 
+# Create an array of values using the linear regression model and pass it in to evaluate the model
 model = [(m*x+b) for x in xs]
 coefficient_of_determination = evaluate_model(model, ys)
 
 print(m, b, coefficient_of_determination)
 
+# creates x and y values that are used to plot line of best fit
 xplot = np.linspace(0, len(xs), 100)
 y = m*xplot+b
 
+# Plot the data and line of best fit
 plt.plot(xplot, y)
 plt.scatter(xs, ys)
 plt.show()
